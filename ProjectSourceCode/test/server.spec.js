10da8e9ff64662729b733a6dced5fc9e8ad75485
Bryan Steigerwald <bryan12061206@gmail.com>
2024-04-08 00:12:18 -0600

    worked on lab-11

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -30,3 +30,45 @@ describe('Server!', () => {
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
 // ********************************************************************************
+
+//positive test case
+// API: /register
+// Input: {username: 'testuser', password: 'password123'}
+// Expect: res.status == 200 and res.body.message == 'Success'
+// Result: This test case should pass and return a status 200 along with a "Success" message.
+// Explanation: The testcase will call the /register API with the following valid inputs
+// and expects the API to return a status of 200 with along with the "Success" message.
+describe('Testing Register API', () => {
+  it('positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ username: 'testuser', password: 'password123' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
+  });
+});
+
+//negative testcase
+// API: /register
+// Input: {username: 'testuser2', password: ''}
+// Expect: res.status == 400 and res.body.message == 'Password cannot be empty'
+// Result: This test case should pass and return a status 400 along with a "Password cannot be empty" message.
+// Explanation: The testcase will call the /register API with the following invalid inputs
+// and expects the API to return a status of 400 with along with the "Password cannot be empty" message.
+describe('Testing Register API', () => {
+  it('Negative : /register. Checking invalid password', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ username: 'testuser2', password: '' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Password cannot be empty');
+        done();
+      });
+  });
+});
