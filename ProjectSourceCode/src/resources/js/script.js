48d1a25496fbf0abdad36921445facc9a1c4d7c7
Jonathan Tang <jota3487@colorado.edu>
2024-04-13 11:40:23 -0600

    Jonathan -- Added a Cart button on nav bar

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,1 +1,20 @@
+// {{!-- Get the cart modal and the cart button --}}
+var modal = document.getElementById("cartModal");
+var cartButton = document.getElementById("cartButton");
 
+// {{!-- When the user clicks on the cart button, open the modal --}}
+cartButton.onclick = function() {
+  modal.style.display = "block";
+}
+
+// {{!-- When the user clicks on <span> (x), close the modal --}}
+document.getElementsByClassName("close")[0].onclick = function() {
+  modal.style.display = "none";
+}
+
+// {{!-- When the user clicks anywhere outside of the modal, close it --}}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
